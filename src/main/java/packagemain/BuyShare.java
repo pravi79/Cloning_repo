2358576b9e9eee0c9c3d66fe0a42f1d238f3dab4
pravin <pravinpatankar79@gmail.com>
2021-09-06 15:03:25 +0530

    buy and sell methods

--- src/main/java/packagemain/BuyShare.java
+++ src/main/java/packagemain/BuyShare.java
@@ -5,4 +5,12 @@ public class BuyShare {
 	public static void main(String[] args) {
 		System.out.println("Velocity group 12");
 	}
+
+	public void addBuyMethod() {
+		System.out.println("buy method added by pravin");
+	}
+
+	public void addSellMethod() {
+		System.out.println("Sell method added by pravin");
+	}
 }
